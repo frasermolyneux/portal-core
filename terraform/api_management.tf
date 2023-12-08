6a140271fea9616ab61a26e4784ecabfd99874f4
Fraser Molyneux <frasermolyneux@mx-mail.io>
2023-12-08 16:42:04 +0000

    Adding API management resource

---
+++ terraform/api_management.tf
@@ -0,0 +1,10 @@
+resource "azurerm_api_management" "apim" {
+  name                = local.api_management_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  publisher_name  = "XtremeIdiots"
+  publisher_email = "admin@xtremeidiots.com"
+
+  sku_name = "Consumption_0"
+}
--- terraform/locals.tf
+++ terraform/locals.tf
@@ -2,4 +2,5 @@ locals {
   resource_group_name   = "rg-portal-core-${var.environment}-${var.location}-${var.instance}"
   app_insights_name     = "ai-portal-core-${var.environment}-${var.location}-${var.instance}"
   app_service_plan_name = "asp-portal-core-${var.environment}-${var.location}-${var.instance}"
+  api_management_name   = "apim-portal-core-${var.environment}-${var.location}-${var.instance}-${random_id.environment_id.hex}"
 }
