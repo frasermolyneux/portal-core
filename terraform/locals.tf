c0b10774a4394a70fce0ef9da021518649845e39
Fraser Molyneux <frasermolyneux@mx-mail.io>
2023-12-08 16:38:53 +0000

    Adding app service plan resource

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,4 +1,5 @@
 locals {
-  resource_group_name = "rg-portal-core-${var.environment}-${var.location}-${var.instance}"
-  app_insights_name   = "ai-portal-core-${var.environment}-${var.location}-${var.instance}"
+  resource_group_name   = "rg-portal-core-${var.environment}-${var.location}-${var.instance}"
+  app_insights_name     = "ai-portal-core-${var.environment}-${var.location}-${var.instance}"
+  app_service_plan_name = "asp-portal-core-${var.environment}-${var.location}-${var.instance}"
 }
