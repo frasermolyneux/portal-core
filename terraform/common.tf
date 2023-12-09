6b6b57e25a6bbcb2ea9feb15fafae5b4e522e742
Fraser Molyneux <frasermolyneux@mx-mail.io>
2023-12-09 13:03:43 +0000

    Experiment with Terraform managed lock

--- terraform/common.tf
+++ terraform/common.tf
@@ -4,3 +4,10 @@ resource "azurerm_resource_group" "rg" {
 
   tags = var.tags
 }
+
+resource "azurerm_management_lock" "rg_lock" {
+  name       = "Terraform Lock - ${random_id.lock.hex}"
+  scope      = azurerm_resource_group.rg.id
+  lock_level = "ReadOnly"
+  notes      = "Lock managed by Terraform to prevent manual change or accidental deletion of the resource group or resources"
+}
