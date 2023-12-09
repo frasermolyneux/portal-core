1ecbba9dd70163f144a6359c5e0329ef26e6e3d2
Fraser Molyneux <frasermolyneux@mx-mail.io>
2023-12-09 13:12:57 +0000

    Changing to a CanNotDelete lock-type

--- terraform/common.tf
+++ terraform/common.tf
@@ -6,8 +6,8 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_management_lock" "rg_lock" {
-  name       = "Terraform Lock - ${random_id.lock.hex}"
+  name       = "Terraform (CanNotDelete) - ${random_id.lock.hex}"
   scope      = azurerm_resource_group.rg.id
-  lock_level = "ReadOnly"
-  notes      = "Lock managed by Terraform to prevent manual change or accidental deletion of the resource group or resources"
+  lock_level = "CanNotDelete"
+  notes      = "CanNotDelete Lock managed by Terraform to prevent manual or accidental deletion of resource group and resources"
 }
