16688759acb702c8db1e7bcbb65f527292e24cc7
Fraser Molyneux <frasermolyneux@mx-mail.io>
2024-05-20 20:29:15 +0100

    Refactor

--- terraform/sql_server.tf
+++ terraform/sql_server.tf
@@ -14,26 +14,6 @@ resource "random_id" "username_suffix" {
   byte_length = 4
 }
 
-resource "azurerm_key_vault" "sql_kv" {
-  name                = local.sql_key_vault_name
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  tenant_id           = data.azurerm_client_config.current.tenant_id
-
-  tags = var.tags
-
-  soft_delete_retention_days = 90
-  purge_protection_enabled   = true
-  enable_rbac_authorization  = true
-
-  sku_name = "standard"
-
-  network_acls {
-    bypass         = "AzureServices"
-    default_action = "Allow"
-  }
-}
-
 resource "azurerm_key_vault_secret" "sql_username" {
   name         = "${local.sql_name}-username"
   value        = "addy${random_id.username_suffix.hex}"
