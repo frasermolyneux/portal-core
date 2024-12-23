db02f315cb06226863688f99197b7c5d5440c8fe
Fraser Molyneux <frasermolyneux@mx-mail.io>
2024-12-23 21:26:57 +0000

    set of strings

--- terraform/resource_health_alerts.tf
+++ terraform/resource_health_alerts.tf
@@ -11,7 +11,7 @@ resource "azurerm_monitor_activity_log_alert" "rg_resource_health" {
     category = "ResourceHealth"
 
     resource_health {
-      previous = "Available"
+      previous = ["Available"]
     }
   }
 
