50693652b38832b8d2de9c0d0b1d17485225deb2
Fraser Molyneux <frasermolyneux@mx-mail.io>
2025-03-19 04:43:47 +0000

    Enable IP masking for Azure Application Insights

--- terraform/app_insights.tf
+++ terraform/app_insights.tf
@@ -5,4 +5,6 @@ resource "azurerm_application_insights" "ai" {
   workspace_id        = "/subscriptions/${var.log_analytics_subscription_id}/resourceGroups/${var.log_analytics_resource_group_name}/providers/Microsoft.OperationalInsights/workspaces/${var.log_analytics_workspace_name}"
 
   application_type = "web"
+
+  disable_ip_masking = true
 }
