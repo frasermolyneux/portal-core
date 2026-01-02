94f9084bf09237e948a5fbb5b608cf032cf0f1eb
Fraser Molyneux <frasermolyneux@mx-mail.io>
2026-01-02 21:45:51 +0000

    Uncomment Application Insights resource configuration

--- terraform/app_insights.tf
+++ terraform/app_insights.tf
@@ -1,12 +1,12 @@
-#resource "azurerm_application_insights" "ai" {
-#  name = local.app_insights_name
-#
-#  resource_group_name = data.azurerm_resource_group.rg.name
-#  location            = data.azurerm_resource_group.rg.location
-#
-#  workspace_id = local.platform_monitoring_workspace_id
-#
-#  application_type = "web"
-#
-#  disable_ip_masking = true
-#}
+resource "azurerm_application_insights" "ai" {
+  name = local.app_insights_name
+
+  resource_group_name = data.azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
+
+  workspace_id = local.platform_monitoring_workspace_id
+
+  application_type = "web"
+
+  disable_ip_masking = true
+}
