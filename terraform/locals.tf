e8741c808ca71490fc8ec65f444bccd54f14f0df
Fraser Molyneux <frasermolyneux@mx-mail.io>
2026-01-03 18:18:52 +0000

    Refactor key vault name formatting for consistency and length limitation

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -25,7 +25,7 @@ locals {
   app_insights_name     = "ai-portal-core-${var.environment}-${var.location}"
   api_management_name   = "apim-portal-core-${var.environment}-${var.location}-${random_id.environment_id.hex}"
   app_service_plan_name = "asp-portal-core-${var.environment}-${var.location}"
-  key_vault_name        = "kv-${random_id.environment_id.hex}-${var.location}"
+  key_vault_name        = substr(format("kv-%s-%s", random_id.environment_id.hex, var.location), 0, 24)
   sql_server_name       = "sql-portal-core-${var.environment}-${var.location}-${random_id.environment_id.hex}"
   dashboard_name        = "portal-core-${var.environment}"
 }
