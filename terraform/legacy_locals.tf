caed26399ff80c64202acc91d541ae226f3aac35
Fraser Molyneux <frasermolyneux@mx-mail.io>
2026-01-03 18:22:21 +0000

    Refactor legacy key vault name formatting for consistency and length limitation

--- terraform/legacy_locals.tf
+++ terraform/legacy_locals.tf
@@ -4,6 +4,6 @@ locals {
   legacy_app_service_plan_name = "asp-portal-core-${var.environment}-${var.location}-${var.instance}"
   legacy_api_management_name   = "apim-portal-core-${var.environment}-${var.location}-${var.instance}-${random_id.legacy_environment_id.hex}"
   legacy_sql_server_name       = "sql-portal-core-${var.environment}-${var.location}-${var.instance}-${random_id.legacy_environment_id.hex}"
-  legacy_key_vault_name        = "kv-${random_id.legacy_environment_id.hex}-${var.location}"
+  legacy_key_vault_name        = substr(format("kv-%s-%s", random_id.legacy_environment_id.hex, var.location), 0, 24)
   legacy_dashboard_name        = "portal-core-${var.environment}-${var.instance}"
 }
