cc64114260613427ccaaec1a5762282f37c300fc
Fraser Molyneux <frasermolyneux@mx-mail.io>
2026-01-05 20:52:27 +0000

    Add API Management and Event Ingest API resources with JWT validation policies

---
+++ terraform/legacy_api_management_event_ingest.tf
@@ -0,0 +1,61 @@
+locals {
+  legacy_event_ingest_api_identifier_uri = data.terraform_remote_state.portal_environments.outputs.event_ingest_api.application.primary_identifier_uri
+}
+
+resource "azurerm_api_management_api_version_set" "legacy_event_ingest_api" {
+  name                = "event-ingest-api"
+  resource_group_name = azurerm_api_management.legacy_apim.resource_group_name
+  api_management_name = azurerm_api_management.legacy_apim.name
+
+  display_name      = "Event Ingest API"
+  versioning_scheme = "Segment"
+}
+
+resource "azurerm_api_management_product" "legacy_event_ingest_api" {
+  product_id          = "event-ingest-api"
+  resource_group_name = azurerm_api_management.legacy_apim.resource_group_name
+  api_management_name = azurerm_api_management.legacy_apim.name
+
+  display_name = "Event Ingest API"
+
+  subscription_required = true
+  approval_required     = false
+  published             = true
+}
+
+resource "azurerm_api_management_product_policy" "legacy_event_ingest_api" {
+  product_id          = azurerm_api_management_product.legacy_event_ingest_api.product_id
+  resource_group_name = azurerm_api_management.legacy_apim.resource_group_name
+  api_management_name = azurerm_api_management.legacy_apim.name
+
+  xml_content = <<XML
+<policies>
+  <inbound>
+      <base/>
+      <cache-lookup vary-by-developer="false" vary-by-developer-groups="false" downstream-caching-type="none" />
+      <validate-jwt header-name="Authorization" failed-validation-httpcode="401" failed-validation-error-message="JWT validation was unsuccessful" require-expiration-time="true" require-scheme="Bearer" require-signed-tokens="true">
+          <openid-config url="https://login.microsoftonline.com/${data.azuread_client_config.current.tenant_id}/v2.0/.well-known/openid-configuration" />
+          <audiences>
+          <audience>${local.legacy_event_ingest_api_identifier_uri}</audience>
+          </audiences>
+          <issuers>
+              <issuer>https://sts.windows.net/${data.azuread_client_config.current.tenant_id}/</issuer>
+          </issuers>
+          <required-claims>
+              <claim name="roles" match="any">
+                <value>EventGenerator</value>
+              </claim>
+          </required-claims>
+      </validate-jwt>
+  </inbound>
+  <backend>
+      <forward-request />
+  </backend>
+  <outbound>
+      <base/>
+      <cache-store duration="3600" />
+  </outbound>
+  <on-error />
+</policies>
+XML
+}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,3 +5,33 @@ output "resource_group_name" {
 output "staging_dashboard_name" {
   value = var.environment == "dev" ? azurerm_portal_dashboard.legacy_staging_dashboard[0].name : ""
 }
+
+output "api_management" {
+  value = {
+    name                = azurerm_api_management.legacy_apim.name
+    id                  = azurerm_api_management.legacy_apim.id
+    resource_group_name = azurerm_api_management.legacy_apim.resource_group_name
+  }
+}
+
+output "api_management_identity" {
+  value = {
+    id           = local.core_api_management_identity.id
+    client_id    = local.core_api_management_identity.client_id
+    principal_id = local.core_api_management_identity.principal_id
+  }
+}
+
+output "app_insights" {
+  value = {
+    name = azurerm_application_insights.legacy_ai.name
+    id   = azurerm_application_insights.legacy_ai.id
+  }
+}
+
+output "event_ingest_api" {
+  value = {
+    version_set_id = azurerm_api_management_api_version_set.legacy_event_ingest_api.id
+    product_id     = azurerm_api_management_product.legacy_event_ingest_api.product_id
+  }
+}
